65988a9b4f48c7877d10da2e8ca66801bd8ab77d
Otávio Augusto Dellay Secco <otavio_delai@estudante.sc.senai.br>
2019-12-06 15:13:10 -0300

    Added Stickers + Fixed Movement Bug

--- src/MeoPomQueimon/hello-world-structured.js
+++ src/MeoPomQueimon/hello-world-structured.js
@@ -9,10 +9,18 @@ light.position.set( 0, 10, -10 );
 scene.add( light );
 light.castShadow = true;
 
-// var ground = new THREE.Mesh (new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load('./ground.jfif')}));
+// var testCube = new THREE.Mesh( 
+//         new THREE.BoxGeometry(9,9,9),
+//         new THREE.MeshBasicMaterial({color:0x00ffff, depthWrite: true}));
+// scene.add(testCube);
+// var testCube;
+
+
+
+
 var ground = new THREE.Mesh (new THREE.PlaneBufferGeometry(2000, 2000), 
                              new THREE.MeshPhongMaterial({color:0xff00ff, 
-                             depthWrite: true}));
+                             depthWrite: false}));
 ground.rotation.x = - Math.PI / 2;
 ground.position.y = -3.5;
 ground.receiveShadow = true;
@@ -31,15 +39,20 @@ camera.position.y = 4;
 
 var sentido = [x = 1, y = 1, z = 1];
 var profes = [
-    {nome: "Padilha", geometry: new THREE.BoxGeometry(1, 1, 1), cube: null, sentido, timerPadilha: 0, sentidoPadilha: 1},
-    {nome: "Marco", geometry: new THREE.BoxGeometry(1, 1, 1), cube: null, sentido},
-    {nome: "Edson", geometry: new THREE.BoxGeometry(1, 1, 1), cube: null, sentido},
+    {nome: "Padilha", geometry: new THREE.BoxGeometry(1, 1, 1), cube: null, sentido: [1,1,1], timerPadilha: 0, sentidoPadilha: 1},
+    {nome: "Marco", geometry: new THREE.BoxGeometry(1, 1, 1), cube: null, sentido: [1,1,1]},
+    {nome: "Edson", geometry: new THREE.BoxGeometry(1, 1, 1), cube: null, sentido: [1,1,1]},
+    {nome: "Caimi", geometry: new THREE.BoxGeometry(1, 1, 1), cube: null, sentido: [1,1,1]},
 
 ];
 var textures = [
     './padilha.webp',
     './marco.jpg',
-    './edson.webp'
+    './edson.webp',
+    './caimi.webp',
+    './caimi1.webp',
+    './caimi2webp',
+    './caimi3webp'
 ];
 
 for(var i = 0; i < profes.length; i++) {
@@ -54,16 +67,19 @@ for(var i = 0; i < profes.length; i++) {
 
 var animate = function() {
     requestAnimationFrame(animate);
+    
     animateProfes();
     animatePadilha();
+
     renderer.render(scene, camera);
 };
 
 animateProfes = function(){
     for(var i = 0; i < profes.length; i++) {
-        profes[i].cube.position.x += 0.01 * profes[i].sentido[0];
-        profes[i].cube.position.y += 0.01 * profes[i].sentido[1];
-        profes[i].cube.position.z += 0.01 * profes[i].sentido[2];
+        profes[i].cube.position.x += (0.01 * ((i+1)/10)) * profes[i].sentido[0];
+        profes[i].cube.position.y += (0.01 * ((i+1)/10)) * profes[i].sentido[1];
+        profes[i].cube.position.z += (0.01 * ((i+1)/10)) * profes[i].sentido[2];
+       
         if(profes[i].cube.position.x >= 3){
             profes[i].sentido[0] *= -1;
             profes[i].cube.position.x += 0.01 * profes[i].sentido[0];
@@ -108,24 +124,23 @@ animate();
 function onKeyDown(event) {
     var keyCode = event.which;
     var speed = 0.1;
-    console.log('keyCode', keyCode);
-    if (keyCode == 87) {
-        // padilha.position.y += speed;
-    } else if (keyCode == 83) {
-        // padilha.position.y -= speed;
-    } else if (keyCode == 65) {
-        // padilha.position.x -= speed;
-    } else if (keyCode == 68) {
-        // padilha.position.x += speed;
-    } else if (keyCode == 32) {
-        // padilha.position.set(0, 0, 0);
+    for(var i = 0; i < profes.length; i++) {
+        console.log('keyCode', keyCode);
+        if (keyCode == 87) {
+            profes[i].cube.position.y += speed;
+        } else if (keyCode == 83) {
+            profes[i].cube.position.y -= speed;
+        } else if (keyCode == 65) {
+            profes[i].cube.position.x -= speed;
+        } else if (keyCode == 68) {
+            profes[i].cube.position.x += speed;
+        } else if (keyCode == 81) {
+            profes[i].cube.position.z -= speed;
+        } else if (keyCode == 69) {
+            profes[i].cube.position.z += speed;
+        } else if (keyCode == 32) {
+            profes[i].cube.position.set(0, 0, 0);
+        }
     }
-    else if (keyCode == 81) {
-        // padilha.position.z -= speed;
-    }
-    else if (keyCode == 69) {
-        // padilha.position.z += speed;
-    }
-
 };
 document.addEventListener("keydown", onKeyDown, false);
